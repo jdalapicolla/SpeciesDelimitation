2b5112aca429961dc431f381856ec999685ae836
Jeronymo Dalapicolla <34402608+jdalapicolla@users.noreply.github.com>
2023-12-11 09:50:28 -0300

    Update E.Repeatability_Morphometric_Data.R

--- 02.RScripts/E.Repeatability_Morphometric_Data.R
+++ 02.RScripts/E.Repeatability_Morphometric_Data.R
@@ -1,15 +1,51 @@
-############################################################################################
-######################### Repeatability for Morphometric Data  #############################
-############################## Jeronymo Dalapicolla, 2019 ##################################
-############################################################################################
+######################## SPECIES DELIMITATION ANALYSES ########################
+################################## TUTORIAL ###################################
 
-############################################################################################
-################################## JERONYMO DALAPICOLLA, 2019 ##############################
-############################### LABORATORIO DE MAMIFEROS - LAMA ############################
-############################## ESALQ - UNIVERSIDADE DE SAO PAULO ###########################
-############################################################################################
 
-##1. FOR OTHER STEPS, PLEASE GO TO: https://github.com/jdalapicolla/
+######################### JERONYMO DALAPICOLLA, 2021 ##########################
+###########################     Repeatability    ##############################
+######################## STEP 01: MORPHOLOGICAL DATA ##########################
+
+##1. CREATE AUXILIARY FUNCTIONS:
+repetibilidade = function (x, data)
+{
+   
+  if("ICC" %in% rownames(installed.packages()) == FALSE){install.packages("ICC")
+  } else {print (paste0("'ICC' ja instalado na biblioteca"))}
+  
+  library(ICC)
+  
+  if (dir.exists(file.path(getwd(), "Repetibilidade")) == FALSE) 
+  {
+    dir.create("Repetibilidade", showWarnings = FALSE)
+  }
+  
+  setwd(file.path(getwd(), "Repetibilidade"))
+  
+  
+  variavel=colnames(data)
+  variavel=variavel[-1]
+  n=length(variavel)
+  
+  resultados=matrix(data = NA, nrow = 7, ncol= n, byrow = FALSE, dimnames = NULL)
+  row.names(resultados) = c("ICC", "LowerCI", "UpperCI", "N", "k", "varw", "vara")
+  colnames(resultados) = c(variavel)
+  
+  for (i in variavel){
+    resul=ICCest(x, i, data, alpha = 0.05, CI.type = "Smith")
+    resultados[1,i] = resul$ICC
+    resultados[2,i] = resul$LowerCI
+    resultados[3,i] = resul$UpperCI
+    resultados[4,i] = resul$N
+    resultados[5,i] = resul$k
+    resultados[6,i] = resul$varw
+    resultados[7,i] = resul$vara
+  }
+  write.table(resultados, file=paste("Repetibilidade_Tabela", "csv", sep="."), sep=",", col.names = NA, row.names = T)
+  setwd('..')
+  
+  return(resultados)
+}
 
 ##2.INPUTS FOR THIS ANALYSIS:
 #A. None in the first step to figure out the number of repeats.
@@ -64,7 +100,7 @@ head(dados)
 #MJ007	6.1	9.21	20.05	16.79	42.01	15.88	15.23	11.45	13.09
 
 
-##7. LOAD THE FUNCTION FOR THIS SCRIPT. THE 'R' FILE MUST BE IN THE FOLDER CHOSE ON THE PREVIOUS STEP.
+##7. LOAD THE FUNCTION FOR THIS SCRIPT:
 source("repeatability.R")
 
 ##8. CALCULATE THE REPEATIBILITY:
@@ -73,4 +109,4 @@ repetibilidade(dados$Sample, dados)
 #dados$Sample = columns with specimens ID
 #dados = table
 
-#The results will be save in the folder under the name: "Repetibilidade_Tabela.csv".
+#The results will be saved in the folder under the name: "Repetibilidade_Tabela.csv".
